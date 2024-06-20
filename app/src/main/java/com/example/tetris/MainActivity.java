6010c771a4c68adc344fb2d1898bddaae2bff2d2
18851066331 <100297254+18851066331@users.noreply.github.com>
2024-06-20 19:37:08 +0800

    MainActivity
    主页java和xml代码

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button startButton;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 找到按钮
+        startButton = findViewById(R.id.start_button);
+        // 设置点击事件监听器
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //后续需要传入配置速度
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
